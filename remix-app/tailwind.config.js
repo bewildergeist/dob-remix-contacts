771ad3e03a882bd317ba49958bc172a923251ecf
Dan Okkels Brendstrup <dob@eaaa.dk>
2024-02-07 22:44:58 +0100

    refactor: Tailwind config ts => js

--- remix-app/tailwind.config.js
+++ remix-app/tailwind.config.js
@@ -1,10 +1,8 @@
-import type { Config } from 'tailwindcss'
-
 export default {
     content: ["./app/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {},
     },
     plugins: [],
-} satisfies Config;
+}
 
